5b67f91fa12118befa60bb36c6886dad900de0bb
Michael Fitzgerald <17684097+mikitz@users.noreply.github.com>
2022-03-06 22:06:51 +0900

    started working with Statblock5e

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Declare a function to select a random value from a dictionary
+// Source: https://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
+var randomProperty = function (obj) {
+    var keys = Object.keys(obj);
+    return obj[keys[ keys.length * Math.random() << 0]];
+}
 // Function to generate a random ID
 // Source: https://gist.github.com/gordonbrander/2230317
 function generateUniqueID() {
@@ -47,11 +53,74 @@ function generateMonster(){
     // Generate a monster based on Challenge Rating
     if (method == 'CR') {
         const cr = document.getElementById('cr').value
-    } 
+        fetch(`data/WOTC_5e_SRD_v5.1/monsters.json`)
+            .then(response => {
+                return response.json();
+            })
+            .then(jsondata => {
+                let monsters = jsondata.filter(i => i.challenge_rating === cr)
+                let monster = randomProperty(monsters)
+                console.log("Monster:", monster)
+                document.getElementById('monster-name').innerText = monster.name // Set the Monster's name
+                document.getElementById('monster-properties').innerText = `${monster.size} ${monster.type}, ${monster.alignment}` // Set its properties
+                document.getElementById('armor-class').innerText = `${monster.armor_class}`
+                if (monster.armor_desc != null) { document.getElementById('armor-class').innerText += ` (${monster.armor_desc})` }
+                document.getElementById('hit-points').innerText = `${monster.hit_points} (${monster.hit_dice})` 
+                document.getElementById('speed').innerText = monster.speed
+                // document.getElementById('str').innerText = monster.strength
+                // document.getElementById('dex').innerText = monster.dexterity
+                // document.getElementById('con').innerText = monster.constitution
+                // document.getElementById('int').innerText = monster.intelligence
+                // document.getElementById('wis').innerText = monster.wisdom
+                // document.getElementById('cha').innerText = monster.charisma
+                if (monster.damage_immunities != null) { document.getElementById('damage-immunities').innerText = monster.damage_immunities }
+                else { 
+                    let elements = document.getElementsByName('damage-immunities') 
+                    elements.forEach(element => {
+                        element.style.display = 'none'
+                    });
+                }
+                if (monster.condition_immunities != null) { document.getElementById('condition-immunities').innerText = monster.condition_immunities }
+                else { 
+                    let elements = document.getElementsByName('damage-immunities') 
+                    elements.forEach(element => {
+                        element.style.display = 'none'
+                    });
+                }
+                if (monster.senses != null) { document.getElementById('senses').innerText = monster.senses }
+                else { 
+                    let elements = document.getElementsByName('damage-immunities') 
+                    elements.forEach(element => {
+                        element.style.display = 'none'
+                    });
+                }
+                if (monster.languages != null) { document.getElementById('senses').innerText = monster.senses }
+                else { 
+                    let elements = document.getElementsByName('damage-immunities') 
+                    elements.forEach(element => {
+                        element.style.display = 'none'
+                    });
+                }
+
+
+
+
+                document.getElementById('output').style.display = 'block'
+            })
+    }
     // Generate a monster based on party composition
     else if (method == 'Party') {
         const difficulty = getSelectedValueFromRadioGroup('difficulty')
-        const PCs = (document.getElementById('player-characters').innerText).split(", ")
+        const pcElements = document.getElementsByClassName('player-character')
+        console.log("PC Elements:", pcElements)
+        let PCs = []
+        for (let index = 0; index < pcElements.length; index++) {
+            const element = pcElements[index];
+            const PC = (element.innerText).replace(" ", "").replace("Level", "").replace(" ", "")
+            PCs.push(PC)
+        }
+        console.log("PCs:", PCs)
+        console.log("Difficulty:", difficulty)
     }
 }
 // Function to add a PC to the party
